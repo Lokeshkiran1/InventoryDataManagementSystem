b6c9041e9fc4cde7360c74c6bf66861b35a9350b
LokeshKiran <lokeshkskiran@gmail.com>
2022-09-14 20:47:00 +0530

    [Lokesh]Added:JSON Inventory Data Management of Rice, Pulses and Wheats

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath= "C:\\Users\\HP\\Desktop\\csharp\\InventoryDataManagement\\jsconfig1.json";
+            FetchForJsonRiceData fetch = new FetchForJsonRiceData();
+            Rice data = fetch.Read(filePath);
+            /* Console.WriteLine(data.typeOfRice.name);
+             Console.WriteLine(data.typeOfRice.weight);
+             Console.WriteLine(data.typeOfRice.price);*/
+            Console.WriteLine("types of rice data");
+           for(int i=0;i<data.typeOfRice.Count;i++)
+            {
+                Console.WriteLine(data.typeOfRice[i].name);
+                Console.WriteLine(data.typeOfRice[i].weight);
+                Console.WriteLine(data.typeOfRice[i].price);
+
+                Console.WriteLine("-----------------------");
+            }
+
+            Console.WriteLine("types of pulse data");
+            for (int i = 0; i < data.typeOfPulse.Count; i++)
+            {
+                Console.WriteLine(data.typeOfPulse[i].name);
+                Console.WriteLine(data.typeOfPulse[i].weight);
+                Console.WriteLine(data.typeOfPulse[i].price);
+
+                Console.WriteLine("-----------------------");
+            }
+
+            Console.WriteLine("types of wheat data");
+            for (int i = 0; i < data.typeOfWheat.Count; i++)
+            {
+                Console.WriteLine(data.typeOfWheat[i].name);
+                Console.WriteLine(data.typeOfWheat[i].weight);
+                Console.WriteLine(data.typeOfWheat[i].price);
+
+                Console.WriteLine("-----------------------");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagement
+{
+    public class Rice
+    {
+        public List<TypeOfRice> typeOfRice;
+        public List<TypeOfPulse> typeOfPulse;
+        public List<TypeOfWheat> typeOfWheat ;
+    }
+    public class TypeOfRice
+    {
+        public string name { get; set; }
+        public string weight { get; set; }
+        public string price { get; set; }
+    }
+    public class TypeOfPulse
+    {
+        public string name { get; set; }
+        public string weight { get; set; }
+        public string price { get; set; }
+    }
+    public class TypeOfWheat
+    {
+        public string name { get; set; }
+        public string weight { get; set; }
+        public string price { get; set; }
+    }
+}
